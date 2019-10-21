42788c16445b6aa15a9efafd0e3731eaa5911674
unknown <tanmayrsm@gmail.com>
2019-10-21 17:30:40 +0530

    done

---
+++ src/SortingVisualizer/SortingVisualizer.jsx
@@ -0,0 +1,248 @@
+import React from 'react';
+import {getMergeSortAnimations} from '../sortingAlgorithms/sortingAlgorithms.js';
+import './SortingVisualizer.css';
+import InputRange from 'react-input-range';
+
+import PropTypes from 'prop-types';
+import {withStyles, makeStyles} from '@material-ui/core/styles';
+//import Paper from '@material-ui/core/Paper';
+import Slider from '@material-ui/core/Slider';
+import Typography from '@material-ui/core/Typography';
+import Tooltip from '@material-ui/core/Tooltip';
+
+const useStyles = makeStyles(theme => ({
+  root: {
+    width: 300 + theme.spacing(3) * 2,
+    padding: theme.spacing(3),
+  },
+  margin: {
+    height: theme.spacing(3),
+  },
+}));
+
+function ValueLabelComponent(props) {
+  const {children, open, value} = props;
+
+  const popperRef = React.useRef(null);
+  React.useEffect(() => {
+    if (popperRef.current) {
+      popperRef.current.update();
+    }
+  });
+
+  return (
+    <Tooltip
+      PopperProps={{
+        popperRef,
+      }}
+      open={open}
+      enterTouchDelay={0}
+      placement="top"
+      title={value}>
+      {children}
+    </Tooltip>
+  );
+}
+
+ValueLabelComponent.propTypes = {
+  children: PropTypes.element.isRequired,
+  open: PropTypes.bool.isRequired,
+  value: PropTypes.number.isRequired,
+};
+
+const PrettoSlider = withStyles({
+  root: {
+    color: '#52af77',
+    height: 8,
+  },
+  thumb: {
+    height: 24,
+    width: 24,
+    backgroundColor: '#fff',
+    border: '2px solid currentColor',
+    marginTop: -8,
+    marginLeft: -12,
+    '&:focus,&:hover,&$active': {
+      boxShadow: 'inherit',
+    },
+  },
+  active: {},
+  valueLabel: {
+    left: 'calc(-50% + 4px)',
+  },
+  track: {
+    height: 8,
+    borderRadius: 4,
+  },
+  rail: {
+    height: 8,
+    borderRadius: 4,
+  },
+})(Slider);
+
+// Change this value for the speed of the animations.
+const ANIMATION_SPEED_MS = 1;
+
+// Change this value for the number of bars (value) in the array.
+const NUMBER_OF_ARRAY_BARS = 10;
+
+// This is the main color of the array bars.
+const PRIMARY_COLOR = 'turquoise';
+
+// This is the color of array bars that are being compared throughout the animations.
+const SECONDARY_COLOR = 'red';
+
+export default class SortingVisualizer extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      array: [],
+      value: 9,
+      value2: {min: 2, max: 10},
+    };
+
+    this.handleChange = this.handleChange.bind(this);
+  }
+
+  componentDidMount() {
+    this.resetArray(5);
+  }
+
+  resetArray() {
+    const array = [];
+    for (let i = 0; i < this.state.value; i++) {
+      array.push(randomIntFromInterval(5, 730));
+    }
+    this.setState({array});
+  }
+
+  mergeSort() {
+    const animations = getMergeSortAnimations(this.state.array);
+    for (let i = 0; i < animations.length; i++) {
+      const arrayBars = document.getElementsByClassName('array-bar');
+      const isColorChange = i % 3 !== 2;
+      if (isColorChange) {
+        const [barOneIdx, barTwoIdx] = animations[i];
+        const barOneStyle = arrayBars[barOneIdx].style;
+        const barTwoStyle = arrayBars[barTwoIdx].style;
+        const color = i % 3 === 0 ? SECONDARY_COLOR : PRIMARY_COLOR;
+        setTimeout(() => {
+          barOneStyle.backgroundColor = color;
+          barTwoStyle.backgroundColor = color;
+        }, i * ANIMATION_SPEED_MS);
+      } else {
+        setTimeout(() => {
+          const [barOneIdx, newHeight] = animations[i];
+          const barOneStyle = arrayBars[barOneIdx].style;
+          barOneStyle.height = `${newHeight}px`;
+        }, i * ANIMATION_SPEED_MS);
+      }
+    }
+  }
+
+  quickSort() {
+    // We leave it as an exercise to the viewer of this code to implement this method.
+  }
+
+  heapSort() {
+    // We leave it as an exercise to the viewer of this code to implement this method.
+  }
+
+  bubbleSort() {
+    // We leave it as an exercise to the viewer of this code to implement this method.
+  }
+
+  // NOTE: This method will only work if your sorting algorithms actually return
+  // the sorted arrays; if they return the animations (as they currently do), then
+  // this method will be broken.
+  testSortingAlgorithms() {
+    for (let i = 0; i < 100; i++) {
+      const array = [];
+      const length = randomIntFromInterval(1, 1000);
+      for (let i = 0; i < length; i++) {
+        array.push(randomIntFromInterval(-1000, 1000));
+      }
+      const javaScriptSortedArray = array.slice().sort((a, b) => a - b);
+      const mergeSortedArray = getMergeSortAnimations(array.slice());
+      console.log(arraysAreEqual(javaScriptSortedArray, mergeSortedArray));
+    }
+  }
+
+  handleChange(name, event) {
+    this.setState({value: event.target.value});
+    console.log('val:' + this.state.value);
+    this.resetArray();
+  }
+
+  render() {
+    const {array} = this.state;
+    //const classes = useStyles();
+
+    return (
+      <div>
+        <div>
+          <button onClick={() => this.resetArray()}>Generate New Array</button>
+          <button onClick={() => this.mergeSort()}>Merge Sort</button>
+          <button onClick={() => this.quickSort()}>Quick Sort</button>
+          <button onClick={() => this.heapSort()}>Heap Sort</button>
+          <button onClick={() => this.bubbleSort()}>Bubble Sort</button>
+          <button onClick={() => this.testSortingAlgorithms()}>
+            Test Sorting Algorithms (BROKEN)
+          </button>
+          <br />
+          <input
+            type="range"
+            value={this.state.value}
+            min="3"
+            max="300"
+            step="2"
+            onChange={this.handleChange.bind(this, 'first')}
+          />
+        </div>
+        <div className="array-container">
+          {array.map((value, idx) => (
+            <div
+              className="array-bar"
+              key={idx}
+              style={{
+                backgroundColor: PRIMARY_COLOR,
+                height: `${value}px`,
+              }}></div>
+          ))}
+          <br />
+
+          <br />
+          {/* <Typography gutterBottom>pretto.fr</Typography>
+        <PrettoSlider
+          id="typeinp"
+          valueLabelDisplay="auto"
+          aria-label="pretto slider"
+          value={this.state.value}
+          min="3"
+          max="300"
+          step="2"
+          onChange={this.handleChange.bind(this, 'first')}
+          //onChange={this.onInput.bind(this)}
+        /> */}
+        </div>
+      </div>
+    );
+  }
+}
+
+// From https://stackoverflow.com/questions/4959975/generate-random-number-between-two-numbers-in-javascript
+function randomIntFromInterval(min, max) {
+  // min and max included
+  return Math.floor(Math.random() * (max - min + 1) + min);
+}
+
+function arraysAreEqual(arrayOne, arrayTwo) {
+  if (arrayOne.length !== arrayTwo.length) return false;
+  for (let i = 0; i < arrayOne.length; i++) {
+    if (arrayOne[i] !== arrayTwo[i]) {
+      return false;
+    }
+  }
+  return true;
+}
